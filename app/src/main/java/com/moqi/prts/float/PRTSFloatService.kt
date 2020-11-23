7582fe0d2cedeb74393fe53d62b2c35a0e01b790
moqi-Git <moqi.kaze@gmail.com>
2020-11-23 14:58:41 +0800

    调试悬浮窗

--- app/src/main/java/com/moqi/prts/float/PRTSFloatService.kt
+++ app/src/main/java/com/moqi/prts/float/PRTSFloatService.kt
@@ -1,20 +1,76 @@
 package com.moqi.prts.float
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.graphics.PixelFormat
+import android.os.Build
 import android.os.IBinder
+import android.util.Log
+import android.view.*
+import com.moqi.prts.R
+import kotlinx.android.synthetic.main.view_prts_float.view.*
 
 class PRTSFloatService: Service() {
+
+    private lateinit var mWindowManager: WindowManager
+    private var mFloatView: View? = null
+
+    private var isShowing = false
+
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 
     override fun onCreate() {
         super.onCreate()
+        Log.e("asdfg", "FloatService onCreate")
+        mWindowManager = applicationContext.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        mFloatView = LayoutInflater.from(applicationContext).inflate(R.layout.view_prts_float, null, false)
+        mFloatView?.apply {
+            float_tv_prts.setOnClickListener {
+                stopSelf()
+            }
+        }
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.e("asdfg", "FloatService onStartCommand")
+        if (intent?.getBooleanExtra("float", false) == true){
+            if (!isShowing){
+                showFloatView()
+            }
+        }
+
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (isShowing) {
+            mWindowManager.removeView(mFloatView)
+        }
+    }
+
+    private fun showFloatView(){
+        isShowing = true
+        val lp = WindowManager.LayoutParams().apply {
+            type = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+            } else {
+                WindowManager.LayoutParams.TYPE_PHONE
+            }
+//            gravity = Gravity.START or Gravity.TOP
+            flags = WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS or
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or
+                    WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+            format = PixelFormat.RGBA_8888
+//            x = 0
+//            y = 0
+            width = WindowManager.LayoutParams.MATCH_PARENT
+            height = WindowManager.LayoutParams.MATCH_PARENT
+        }
+        mWindowManager.addView(mFloatView, lp)
     }
 
 }
--- app/src/main/java/com/moqi/prts/ui/home/HomeFragment.kt
+++ app/src/main/java/com/moqi/prts/ui/home/HomeFragment.kt
@@ -2,8 +2,9 @@ package com.moqi.prts.ui.home
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.PixelFormat
+import android.graphics.*
 import android.hardware.display.DisplayManager
+import android.media.Image
 import android.media.ImageReader
 import android.media.projection.MediaProjection
 import android.media.projection.MediaProjectionManager
@@ -21,6 +22,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.moqi.prts.R
 import com.moqi.prts.access.GlobalStatus
+import com.moqi.prts.float.PRTSFloatService
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
@@ -29,6 +31,8 @@ class HomeFragment : Fragment() {
   private lateinit var mm: MediaProjectionManager
   private lateinit var holder: SurfaceHolder
   private lateinit var imageReader: ImageReader
+
+  private var screenShot = false
 
   override fun onCreateView(
     inflater: LayoutInflater,
@@ -42,22 +46,52 @@ class HomeFragment : Fragment() {
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+    mm = requireContext().getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+    startActivityForResult(mm.createScreenCaptureIntent(), 101)
+
     src_tv_start.setOnClickListener {
-      mm = requireContext().getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
-      startActivityForResult(mm.createScreenCaptureIntent(), 101)
+//      Log.e("qwert", "screenshot click")
+//      screenShot = true
+      requireContext().startService(Intent(requireContext(), PRTSFloatService::class.java).apply {
+        putExtra("float", true)
+      })
     }
     imageReader = ImageReader.newInstance(GlobalStatus.screenHeight, GlobalStatus.screenWidth, PixelFormat.RGBA_8888, 2).apply {
       setOnImageAvailableListener({
-        Log.e("asdfg", "OnImageAvailable")
+//        Log.e("asdfg", "OnImageAvailable")
         val image = it.acquireNextImage()
         if (image != null){
           val info = image.planes
-          Log.e("asdfg", "${info}")
+//          Log.e("asdfg", "${info}")
+          if (screenShot){
+            Log.e("qwert", "screenshot start")
+            screenShot(image)
+            Log.e("qwert", "screenshot end")
+            screenShot = false
+          }
           image.close()
         }
       }, null)
     }
-    holder = scr_sv_main.holder
+//    holder = scr_sv_main.holder
+  }
+
+  private fun screenShot(image: Image){
+    logImageInfo(image)
+    val buffer = image.planes[0].buffer
+    val width = image.width
+    val height = image.height
+//    val pixelStride = image.planes[0].pixelStride
+//    val rowStride = image.planes[0].rowStride
+//    val rowPadding = rowStride - pixelStride * width
+
+    val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+    bitmap.copyPixelsFromBuffer(buffer)
+//    val bytes = ByteArray(buffer.capacity())
+//    buffer.get(bytes)
+//    val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+
+    scr_iv_main.setImageBitmap(bitmap)
   }
 
   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -65,6 +99,10 @@ class HomeFragment : Fragment() {
     val projection = mm.getMediaProjection(resultCode, data?:Intent())
 
     projection.createVirtualDisplay("test", GlobalStatus.screenHeight, GlobalStatus.screenWidth,
-    1, DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC, imageReader.surface, null, null)
+    1, DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR, imageReader.surface, null, null)
+  }
+
+  private fun logImageInfo(image: Image){
+    Log.e("asdfg", "format=${image.format}, size=(${image.width}, ${image.height}), rs=${image.planes[0].rowStride}, ps=${image.planes[0].pixelStride}")
   }
 }
